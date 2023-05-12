1fc54612536dee7aa6dffb24439a5aa52f169d8b
Yaroslav <dombrovskiy91@gmail.com>
2023-05-12 13:16:31 +0200

    add selectors req in cmp JobList
    add selectors req in cmp JobList

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
-import "./index.css";
 import {TheHeader} from "./components/TheHeader";
 import {FilterPanel} from "./components/FilterPanel";
 import {JobList} from "./components/JobList";
 
+import "./index.css";
+
+import {useEffect} from "react";
+import {useDispatch} from "react-redux";
+import {addPositions} from "./store/positions/position-actions";
+import data from "./mock/data.json"
+
 export default function App() {
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(addPositions(data));
+  });
   return (
     <div className="App">
       <TheHeader/>
       <div className="container">
       <FilterPanel/>
-      <JobList/>
+      <JobList data={data}/>
       </div>
     </div>
   );
---
+++ src/store/positions/position-actions.js
@@ -0,0 +1,8 @@
+// constants
+export const ADD_POSITIONS = "ADD_POSITIONS";
+
+// action creator
+export const addPositions = (positions) => ({
+  type: ADD_POSITIONS,
+    positions
+});
